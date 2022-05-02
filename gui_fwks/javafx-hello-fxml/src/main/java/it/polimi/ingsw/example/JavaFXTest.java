6d4b46da8b71e4465f49e29de633bb433d19518d
Daniele Cattaneo <daniele.cattaneo@polimi.it>
2022-05-02 23:34:20 +0200

    Remove extra imports in the fxml example.

--- gui_fwks/javafx-hello-fxml/src/main/java/it/polimi/ingsw/example/JavaFXTest.java
+++ gui_fwks/javafx-hello-fxml/src/main/java/it/polimi/ingsw/example/JavaFXTest.java
@@ -1,13 +1,9 @@
 package it.polimi.ingsw.example;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
